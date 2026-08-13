1ad88516000ca86bdce8aa0a7a57c829e558f959
Samuel Pineda Lopez <samuel.pineda@sumimedical.com>
2026-08-13 17:20:14 -0500

    chore: configure proxy trust and sanitize Sanctum stateful domain environment variables

--- backend-novedades/config/sanctum.php
+++ backend-novedades/config/sanctum.php
@@ -18,12 +18,12 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
+    'stateful' => array_map('trim', explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s,%s',
         'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1',
         Sanctum::currentApplicationUrlWithPort(),
         'trapiche.manevoapp.com,api-novedades.manevoapp.com'
-    ))),
+    )))),
 
     /*
     |--------------------------------------------------------------------------
